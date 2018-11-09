14572e1c87f9baa8d2b388942d4480795cbd97d2
Mírian <mirianfrsilva@gmail.com>
2018-11-09 00:52:12 -0200

    class creation

---
+++ Policial.java
@@ -0,0 +1,23 @@
+public class Policial {
+    public String patente;
+    public String nome;
+    private String endereco;
+
+    public Policial(String patente, String nome, String endereco) {
+      this.patente = patente;
+      this.nome = nome;
+      this.endereco = endereco;
+    }
+
+    public void emiteNotificacao(){
+      //TODO;
+    }
+
+    public void preencheDados(){
+      //TODO;
+    }
+
+    public void confirmaAtendimento(){
+      //TODO;
+    }
+}
