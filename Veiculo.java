f5ffbb2849333dd2544d17121481011f6d1ac268
Mírian <mirianfrsilva@gmail.com>
2018-11-09 01:00:45 -0200

    class creation

---
+++ Veiculo.java
@@ -0,0 +1,19 @@
+public abstract class Veiculo {
+    public int capacidadeLugares;
+    public String especialidade;
+    public String localizacao;
+    public boolean disponibilidade;
+    private float combustivel;
+
+    public Veiculo(int capacidadeLugares, String especialidade, String localizacao, boolean disponibilidade, float combustivel){
+      this.capacidadeLugares = capacidadeLugares;
+      this.especialidade = especialidade;
+      this.localizacao = localizacao;
+      this.disponibilidade = disponibilidade;
+      this.combustivel = combustivel;
+    }
+
+    public void enviaVeiculo() {
+        //TODO;
+    }
+}
