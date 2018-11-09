0c7026617dfeedf19c5b1b861ebe02098eacb9cd
Mírian <mirianfrsilva@gmail.com>
2018-11-09 00:53:04 -0200

    class creation

---
+++ Sistema.java
@@ -0,0 +1,13 @@
+public class Sistema {
+    public void notificaPolicial(){
+      //TODO;
+    }
+
+    public void selecionaVeiculo(){
+      //TODO;
+    }
+
+    private void atualizaDisponiveis(){
+      //TODO;
+    }
+}
